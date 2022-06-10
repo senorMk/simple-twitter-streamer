e39a4ff7abb2aeb7e4f33283b0766f4ec2dec5ad
Penjani Mkandawire <mkandawire15@gmail.com>
2022-06-10 13:37:57 +0200

    Rename variable.

--- client/src/components/Rule.js
+++ client/src/components/Rule.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Chip, Container } from "@mui/material";
 
-export const Rule = ({ data, onRuleDelete }) => {
+export const Rule = ({ rule, onRuleDelete }) => {
   return (
     <Container sx={{ p: "5px" }}>
-      <Chip label={data.value} onDelete={() => onRuleDelete(data.id)} />
+      <Chip label={rule.value} onDelete={() => onRuleDelete(rule.id)} />
     </Container>
   );
 };
